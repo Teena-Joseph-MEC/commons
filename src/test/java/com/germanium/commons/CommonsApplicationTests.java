b4a2d05f27229c9c67f6411b2b10baa42a92bdd3
Teena-Joseph-MEC <teenajoseph.mec@gmail.com>
2021-10-19 23:03:17 +0100

    initial commit

---
+++ src/test/java/com/germanium/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanium.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
