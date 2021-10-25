8ee3d5c0aee097fc93546375bc8c469f6063e341
Teena-Joseph-MEC <teenajoseph.mec@gmail.com>
2021-10-25 18:10:52 +0100

    Adding models

---
+++ src/main/resources/db/migration/V1__Create_Table.sql
@@ -0,0 +1,20 @@
+
+
+CREATE TABLE IF NOT EXISTS `notification` (
+    `notification_id` INT NOT NULL,
+    `employee_id`INT NOT NULL,
+    `message` VARCHAR(30) NOT NULL,
+    `is_read` BOOLEAN DEFAULT FALSE,
+    `title` VARCHAR(30) NOT NULL,
+    `date` DATE,
+    PRIMARY KEY(`notification_id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `substitution` (
+	`substitute_id` INT NOT NULL,
+	`leave_id` INT NOT NULL,
+	`date_of_approval` DATE NOT NULL,
+	PRIMARY KEY (`substitute_id`,`leave_id`)
+	FOREIGN KEY (`leave_id`) REFERENCES leave_rules(`leave_id`)
+);
