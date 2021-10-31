42e4d006ee7a7dbfec683449e434fe20c50e1515
ajinpm <36944267+ajinpm@users.noreply.github.com>
2021-10-31 14:55:16 +0000

    changes

---
+++ src/main/java/com/germanium/commons/controller/EmailController.java
@@ -0,0 +1,23 @@
+package com.germanium.commons.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.germanium.commons.models.MailRequest;
+
+@RequestMapping("/mail")
+@RestController
+public class EmailController {
+	
+	Logger logger = LoggerFactory.getLogger(EmailController.class);
+
+	@PostMapping(value = "/send", consumes = "application/json")
+	public MailRequest sendMail(@RequestBody MailRequest request) {
+		logger.info("Request for sending mail received.");
+		return request;
+	}
+}
