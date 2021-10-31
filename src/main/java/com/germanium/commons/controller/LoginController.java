903f8d21b7731af18b2c70543e50f49311e86045
arjunshenoy <arjunkshenoy.mec@gmail.com>
2021-10-31 16:06:40 +0000

    Added basic HTTP authentication mechanisim

--- src/main/java/com/germanium/commons/controller/LoginController.java
+++ src/main/java/com/germanium/commons/controller/LoginController.java
@@ -1,5 +1,39 @@
 package com.germanium.commons.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.germanium.commons.dto.UpdatePasswordDto;
+import com.germanium.commons.models.Login;
+import com.germanium.commons.service.LoginService;
+
+@RestController
 public class LoginController {
+
+	@Autowired
+	LoginService loginservice;
+
+	@PostMapping("/updatePassword")
+	public ResponseEntity<String> changeUserPassword(@Valid @RequestBody UpdatePasswordDto passwordDto)
+			throws Exception {
+		Login login = loginservice
+				.getLoggedinUserDetails(SecurityContextHolder.getContext().getAuthentication().getName());
+
+		if (!loginservice.checkIfValidOldPassword(login, passwordDto.getOldPassword())) {
+			throw new Exception("Invalid Password");
+		}
+
+		loginservice.changeUserPassword(login.getUserName(), passwordDto.getNewPassword());
+		return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.LOCATION).body("Password Updated Successfully");
+
+	}
 
 }
--- src/main/resources/db/migration/V1__Create_Table.sql
+++ src/main/resources/db/migration/V1__Create_Table.sql
@@ -1,5 +1,4 @@
 
-
 CREATE TABLE IF NOT EXISTS `notification` (
     `notification_id` INT NOT NULL,
     `employee_id`INT NOT NULL,
@@ -17,3 +16,18 @@ CREATE TABLE IF NOT EXISTS `substitution` (
 	`date_of_approval` DATE NOT NULL,
 	PRIMARY KEY (`substitute_id`)
 );
+
+CREATE TABLE `login` (
+    `employee_id` INT NOT NULL,
+    `username` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(256) NOT NULL,
+    `isActive` BOOLEAN DEFAULT TRUE,
+    `roles` VARCHAR(512), 
+    `created_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(`username`)
+);
+
+INSERT INTO login (employee_id,username, password,isActive, roles, created_ts) VALUES (1,'admin','admin',true,'ADMIN', current_timestamp());
+INSERT INTO login (employee_id,username, password,isActive, roles, created_ts) VALUES (2,'arjun','qwerty',true,'STAFF', current_timestamp());
+INSERT INTO login (employee_id,username, password,isActive, roles, created_ts) VALUES (3,'chinmay','qwertyu',true,'ADMIN', current_timestamp());
+
